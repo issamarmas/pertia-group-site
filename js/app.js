14213c349c873af9e081dcb127baf62155bb2ef7
issam <almorchid123@gmail.com>
2019-05-28 19:31:47 +0100

    event js

---
+++ js/app.js
@@ -0,0 +1,64 @@
+$(".sub").hover(function () {
+    //stuff to do on mouse enter
+    
+    $('.sub-menu1').css("-webkit-transform", "translateX(10px)")
+    $('.sub-menu1').css("opacity", "1")
+}, 
+function () {
+    $('.sub-menu1').css("-webkit-transform", "translateY(0px)")
+    $('.sub-menu1').css("opacity", "0")
+
+});
+
+$(".lang").hover(function () {
+    //stuff to do on mouse enter
+    
+    $('.lang-items').css("-webkit-transform", "translateX(10px)")
+    $('.lang-items').css("opacity", "1")
+}, 
+function () {
+    $('.lang-items').css("-webkit-transform", "translateY(0px)")
+    $('.lang-items').css("opacity", "0")
+});
+////////////////////
+
+
+$(".mov1").hover(function () {
+    //stuff to do on mouse enter
+
+    $('.ic1').css("-webkit-transform", "rotate(90deg)")
+}, 
+function () {
+    $('.ic1').css("-webkit-transform", "rotate(270deg)")
+});
+/////////////////////////////////////////////
+$(".mov2").hover(function () {
+    //stuff to do on mouse enter
+
+    $('.ic2').css("-webkit-transform", "rotate(90deg)")
+}, 
+function () {
+    $('.ic2').css("-webkit-transform", "rotate(270deg)")
+});
+
+
+///////////////////////////////////////////////////
+
+$(".mov3").hover(function () {
+    //stuff to do on mouse enter
+
+    $('.ic3').css("-webkit-transform", "rotate(90deg)")
+}, 
+function () {
+    $('.ic3').css("-webkit-transform", "rotate(270deg)")
+});
+///////////////////////////////////////////////
+$(".mov4").hover(function () {
+    //stuff to do on mouse enter
+
+    $('.ic4').css("-webkit-transform", "rotate(90deg)")
+}, 
+function () {
+    $('.ic4').css("-webkit-transform", "rotate(270deg)")
+});
+/////////////////
