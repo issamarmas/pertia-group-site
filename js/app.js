95f4a2523cc601e108e9cc4cb88dcbd4c92d6981
issam <almorchid123@gmail.com>
2019-05-28 19:54:28 +0100

    fix rotate icon

--- js/app.js
+++ js/app.js
@@ -1,44 +1,21 @@
-$(".sub").hover(function () {
-    //stuff to do on mouse enter
-    
-    $('.sub-menu1').css("-webkit-transform", "translateX(10px)")
-    $('.sub-menu1').css("opacity", "1")
-}, 
-function () {
-    $('.sub-menu1').css("-webkit-transform", "translateY(0px)")
-    $('.sub-menu1').css("opacity", "0")
-
-});
-
-$(".lang").hover(function () {
-    //stuff to do on mouse enter
-    
-    $('.lang-items').css("-webkit-transform", "translateX(10px)")
-    $('.lang-items').css("opacity", "1")
-}, 
-function () {
-    $('.lang-items').css("-webkit-transform", "translateY(0px)")
-    $('.lang-items').css("opacity", "0")
-});
-////////////////////
 
 
 $(".mov1").hover(function () {
     //stuff to do on mouse enter
 
-    $('.ic1').css("-webkit-transform", "rotate(90deg)")
+    $('.ic1').css("-webkit-transform", "rotate(270deg)")
 }, 
 function () {
-    $('.ic1').css("-webkit-transform", "rotate(270deg)")
+    $('.ic1').css("-webkit-transform", "rotate(90deg)")
 });
 /////////////////////////////////////////////
 $(".mov2").hover(function () {
     //stuff to do on mouse enter
 
-    $('.ic2').css("-webkit-transform", "rotate(90deg)")
+    $('.ic2').css("-webkit-transform", "rotate(270deg)")
 }, 
 function () {
-    $('.ic2').css("-webkit-transform", "rotate(270deg)")
+    $('.ic2').css("-webkit-transform", "rotate(90deg)")
 });
 
 
@@ -47,18 +24,18 @@ function () {
 $(".mov3").hover(function () {
     //stuff to do on mouse enter
 
-    $('.ic3').css("-webkit-transform", "rotate(90deg)")
+    $('.ic3').css("-webkit-transform", "rotate(270deg)")
 }, 
 function () {
-    $('.ic3').css("-webkit-transform", "rotate(270deg)")
+    $('.ic3').css("-webkit-transform", "rotate(90deg)")
 });
 ///////////////////////////////////////////////
 $(".mov4").hover(function () {
     //stuff to do on mouse enter
 
-    $('.ic4').css("-webkit-transform", "rotate(90deg)")
+    $('.ic4').css("-webkit-transform", "rotate(270deg)")
 }, 
 function () {
-    $('.ic4').css("-webkit-transform", "rotate(270deg)")
+    $('.ic4').css("-webkit-transform", "rotate(90deg)")
 });
 /////////////////
