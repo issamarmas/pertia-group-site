063a600b00f5cbbe48d291ac4746a102dcb9d941
issam <almorchid123@gmail.com>
2019-06-02 03:29:23 +0100

    less time js writer

--- js/app.js
+++ js/app.js
@@ -103,7 +103,7 @@ $(function(){
               this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
           
               // Initial Type Speed
-              let typeSpeed = 200;
+              let typeSpeed = 100;
           
               if(this.isDeleting) {
                 typeSpeed /= 2;
